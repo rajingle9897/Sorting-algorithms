deef1e9028db9eaaa314ca6580b2d11f4c4044fd
rajingle9897 <58242268+rajingle9897@users.noreply.github.com>
2020-01-30 20:13:16 -0500

    Add files via upload

---
+++ insertionsort.C
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+void insertionsort()
+{
+	int n, i, j, k, temp, flag = 0;
+	printf("Enter number of elements");
+	scanf("%d", &n);
+	int a[n];
+	clock_t t;
+	t = clock();
+	for (int i = 0; i < n; i++)
+		a[i] = rand();
+
+	for (k = 1; k < n; k++)
+	{
+		temp = a[k];
+		j = k - 1;
+		while (j >= 0 && temp <= a[j])
+		{
+			a[j + 1] = a[j];
+			j = j - 1;
+		}
+		a[j + 1] = temp;
+	}
+
+	t = clock() - t;
+	double time_taken = ((double)t) / CLOCKS_PER_SEC;
+
+	printf("\nSorted sequence:\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+
+	printf("\nInsertion sort took %f seconds to execute \n", time_taken);
+}
+
+
+int main()
+{
+	insertionsort();
+	return 0;
+}
---
+++ mergesort.C
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+void mergesort(int[], int, int);
+void merge(int[], int, int, int);
+int i, n, m;
+void mergesort(int a[], int beg, int end)
+{
+	int mid;
+	if (beg < end)
+	{
+		mid = (beg + end) / 2;
+		mergesort(a, beg, mid);
+		mergesort(a, mid + 1, end);
+		merge(a, beg, mid, end);
+	}
+
+}
+void merge(int a[], int beg, int mid, int end)
+{
+	int i = beg, j = mid + 1, k, x = beg;
+	int temp[m];
+	while (i <= mid && j <= end)
+	{
+		if (a[i] < a[j])
+		{
+			temp[x] = a[i];
+			i = i + 1;
+		}
+		else
+		{
+			temp[x] = a[j];
+			j = j + 1;
+		}
+		x++;
+	}
+	if (i > mid)
+	{
+		while (j <= end)
+		{
+			temp[x] = a[j];
+			x++;
+			j++;
+		}
+	}
+	else
+	{
+		while (i <= mid)
+		{
+			temp[x] = a[i];
+			x++;
+			i++;
+		}
+	}
+	k = beg;
+	while (k < x)
+	{
+		a[k] = temp[k];
+		k++;
+	}
+}
+int main ()
+{	int n;
+	printf("Enter the number of elements");
+	scanf("%d", &n);
+	int a[n];
+	m = n;
+	clock_t t;
+	t = clock();
+	for (int i = 0; i < n; i++)
+		a[i] = rand();
+	mergesort(a, 0, n - 1);
+
+	t = clock() - t;
+	double time_taken = ((double)t) / CLOCKS_PER_SEC;
+
+	printf("\nSorted sequence:\n");
+	for (i = 0; i < n; i++)
+	{
+		printf(" %d ", a[i]);
+	}
+
+	printf("\nMerge Sort took %f seconds to execute \n", time_taken);
+	return 0;
+
+}
