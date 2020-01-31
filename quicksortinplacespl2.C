e56f0fc409e5cabb40b0cd4405ac1812a8bb9b4f
rajingle9897 <58242268+rajingle9897@users.noreply.github.com>
2020-01-30 20:16:59 -0500

    Add files via upload

---
+++ quicksortinplacespl2.C
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int partition(int a[], int beg, int end);
+void quicksort(int a[], int beg, int end);
+
+int partition(int a[], int beg, int end)
+{
+
+	int left, right, temp, piv, c;
+	piv = left = beg;
+	right = end;
+	c = 0;
+	while (c != 1)
+	{
+		while ((a[piv] <= a[right]) && (piv != right))
+			right--;
+		if (piv == right)
+			c = 1;
+		else if (a[piv] > a[right])
+		{
+			temp = a[piv];
+			a[piv] = a[right];
+			a[right] = temp;
+			piv = right;
+		}
+		if (c != 1)
+		{
+			while ((a[piv] >= a[left]) && (piv != left))
+				left++;
+			if (piv == left)
+				c = 1;
+			else if (a[piv] < a[left])
+			{
+				temp = a[piv];
+				a[piv] = a[left];
+				a[left] = temp;
+				piv = left;
+			}
+		}
+	}
+	return piv;
+}
+void quicksort(int a[], int beg, int end)
+{
+
+	int piv;
+	if (beg < end)
+	{
+		piv = partition(a, beg, end);
+		quicksort(a, beg, piv - 1);
+		quicksort(a, piv + 1, end);
+	}
+}
+
+
+int main()
+{
+	int i, n;
+	printf("Enter the no of elements");
+	scanf("%d", &n);
+	int arr[n];
+	clock_t t;
+	t = clock();
+	for (i = 0; i < n; i++)
+		arr[i] = (n - 1) - i;
+	quicksort(arr, 0, n - 1);
+
+
+	t = clock() - t;
+	double time_taken = ((double)t) / CLOCKS_PER_SEC;
+	printf("\nSorted sequence:\n");
+
+	for (i = 0; i < n; i++)
+		printf(" %d\t", arr[i]);
+	printf("\nQuicksortinplace took %f seconds to execute \n", time_taken);
+
+	return 0;
+}
+
---
+++ quicksortmedianfinalspl2.C
@@ -0,0 +1,94 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void *__gxx_personality_v0;
+
+void insertionsort(int a[], int left, int right);
+
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+	return ;
+}
+
+int median(int a[], int left, int right)
+{
+	int center = (left + right) / 2;
+	if (a[center] < a[left])
+		swap(&a[left], &a[center]);
+	if (a[right] < a[left])
+		swap(&a[left], &a[right]);
+	if (a[right] < a[center])
+		swap(&a[center], &a[right]);
+
+	swap(&a[center], &a[right - 1]);
+	return a[right - 1];
+}
+
+void quicksort(int a[], int left, int right)
+{
+	if (left + 10 <= right)
+	{
+		int pivot = median(a, left, right);
+		if (left == right - 1) return;
+		int i = left;
+		int j = right - 1;
+		for ( ; ;)
+		{
+			while (a[++i] < pivot) {}
+			while (pivot < a[--j]) {}
+			if ( i < j)
+				swap(&a[i], &a[j]);
+			else
+				break ;
+		}
+		swap(&a[i], & a[right - 1]);
+		quicksort(a, left, i - 1);
+		quicksort(a, i + 1, right);
+	}
+	else
+	{
+		insertionsort(a, left, right);
+	}
+	return ;
+}
+
+void insertionsort(int a[], int left, int right)
+{
+	int i, j, k, temp;
+
+	for (k = left + 1; k <= right; k++)
+	{
+		temp = a[k];
+		j = k - 1;
+		while (j >= 0 && temp <= a[j])
+		{
+			a[j + 1] = a[j];
+			j = j - 1;
+		}
+		a[j + 1] = temp;
+	}
+}
+
+
+int main()
+{
+	int i, n;
+	printf("Enter the no of elements");
+	scanf("%d", &n);
+	clock_t t;
+	t = clock();
+	int a[n];
+	for (i = 0; i < n; i++)
+		a[i] = (n - 1) - i;
+	quicksort(a, 0, n - 1);
+
+	t = clock() - t;
+	double time_taken = ((double)t) / CLOCKS_PER_SEC;
+	printf("Sorted sequence:");
+	for (i = 0 ; i < n ; i++)
+		printf("%d ", a[i]);
+	printf("\nSorting took %f seconds to execute \n", time_taken);
+}
